aadd221f8129fd6fb49e903cad7d86c821cdd540
Jan Losinski <losinski@wh2.tu-dresden.de>
2014-09-09 21:52:26 +0200

    Add unittest for permission parsing

--- tests/test_backend/test_user.py
+++ tests/test_backend/test_user.py
@@ -1,5 +1,5 @@
 import unittest
-from db_backend.utils.user import exma_passhash, ApiUser, GUEST_MASK
+from db_backend.utils.user import exma_passhash, ApiUser, GUEST_MASK, parse_permissions
 
 
 class TestPasswordHandling(unittest.TestCase):
@@ -24,3 +24,31 @@ class TestApiUser(unittest.TestCase):
     def test_0020_dummy_user_is_guest(self):
         user = ApiUser()
         self.assertEqual(user.perm_masks, GUEST_MASK)
+
+
+class TestParsePermissions(unittest.TestCase):
+    parsed_ref = {'start_perms': {3, 4, 9, 11, 12, 15, 16, 18},
+                  'upload_perms': {3, 4, 9, 11, 12, 15, 16, 18},
+                  'show_perms': {2, 3, 4, 9, 11, 12, 15, 16, 18, 22, 23},
+                  'read_perms': {2, 3, 4, 9, 11, 12, 15, 16, 18, 22, 23},
+                  'reply_perms': {3, 4, 9, 11, 12, 15, 16, 18}}
+
+    def test_0010_parse_byte(self):
+        ref = (b'a:5:{s:11:"start_perms";s:20:"3,9,12,15,16,18,4,11";'
+               b's:11:"reply_perms";s:20:"3,9,12,15,16,18,4,11"'
+               b';s:10:"read_perms";s:28:"2,3,9,12,15,16,18,22,23,4,11";'
+               b's:12:"upload_perms";s:20:"3,9,12,15,16,18,4,11";'
+               b's:10:"show_perms";s:28:"2,3,9,12,15,16,18,22,23,4,11";}')
+        perms = parse_permissions(ref)
+        print(perms)
+        self.assertEqual(perms, self.parsed_ref)
+
+    def test_0020_parse_unicode(self):
+        ref = ('a:5:{s:11:"start_perms";s:20:"3,9,12,15,16,18,4,11";'
+               's:11:"reply_perms";s:20:"3,9,12,15,16,18,4,11"'
+               ';s:10:"read_perms";s:28:"2,3,9,12,15,16,18,22,23,4,11";'
+               's:12:"upload_perms";s:20:"3,9,12,15,16,18,4,11";'
+               's:10:"show_perms";s:28:"2,3,9,12,15,16,18,22,23,4,11";}')
+        perms = parse_permissions(ref)
+        print(perms)
+        self.assertEqual(perms, self.parsed_ref)
