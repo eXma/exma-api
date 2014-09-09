cdbbbf8f8f960ab3c45e2c04fc4d2551e264fe7c
Jan Losinski <losinski@wh2.tu-dresden.de>
2014-09-09 21:21:59 +0200

    Add basic unittest for api user

--- tests/test_backend/test_user.py
+++ tests/test_backend/test_user.py
@@ -1,5 +1,5 @@
 import unittest
-from db_backend.utils.user import exma_passhash
+from db_backend.utils.user import exma_passhash, ApiUser, GUEST_MASK
 
 
 class TestPasswordHandling(unittest.TestCase):
@@ -11,6 +11,16 @@ class TestPasswordHandling(unittest.TestCase):
         hash = exma_passhash("peng", "abc")
         self.assertNotEqual(hash, '57378fa5f7f15da155244b48622b2749')
 
-    def test_0020_invalid_salt(self):
+    def test_0030_invalid_salt(self):
         hash = exma_passhash("test", "peng")
         self.assertNotEqual(hash, '57378fa5f7f15da155244b48622b2749')
+
+
+class TestApiUser(unittest.TestCase):
+    def test_0010_dummy_user_unauthenticated(self):
+        user = ApiUser()
+        self.assertFalse(user.authenticated())
+
+    def test_0020_dummy_user_is_guest(self):
+        user = ApiUser()
+        self.assertEqual(user.perm_masks, GUEST_MASK)
